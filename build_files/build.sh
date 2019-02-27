34871e36e9bd6e2ba053d436ad1b90fe3e88df69
Andy <andy@xaya.io>
2019-02-27 17:08:25 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,15 +1,11 @@
 pacman -S base-devel git mingw-w64-x86_64-toolchain make mingw-w64-x86_64-cmake autoconf-archive mingw-w64-x86_64-protobuf mingw-w64-x86_64-gtest mingw-w64-x86_64-gflags mingw-w64-x86_64-zeromq mingw-w64-x86_64-openssl mingw-w64-x86_64-glog mingw-w64-x86_64-lmdb mingw-w64-x86_64-lmdbxx mingw-w64-x86_64-sqlite3 mingw-w64-x86_64-libmicrohttpd wget
-
 git clone https://github.com/jonathanmarvens/argtable2.git
 cd argtable2
-
 cmake . -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=$MINGW_PREFIX
 make -j2
 make install
 cp ./src/argtable2.h /home/../mingw64/include/argtable2.h
-
 cd ..
-
 git clone https://github.com/xaya/libjson-rpc-cpp
 cd libjson-rpc-cpp
 mkdir win32-deps
@@ -19,16 +15,10 @@ make -j2
 make install
 cd dist
 cp -r ./ /home/../mingw64/
-
-
 Cd ~/
-
 Curl -o /home/../mingw64/lib/pkgconfig/libglog.pc https://raw.githubusercontent.com/xaya/xaya_tutorials/master/build_files/libglog.pc
 Curl -o /home/../mingw64/lib/pkgconfig/lmdb.pc https://raw.githubusercontent.com/xaya/xaya_tutorials/master/build_files/lmdb.pc
-
-
 git clone https://github.com/xaya/libxayagame.git
-
 cd libxayagame
 ./autogen.sh
 ./configure
